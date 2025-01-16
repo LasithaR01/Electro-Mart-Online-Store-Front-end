eac0504ff53cc93770e015bad57cbe1a048bb56d
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-01-16 15:12:22 +0530

    feat: Components connect for route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {ProductsComponent} from './components/products/products.component';
+import {CartComponent} from './components/cart/cart.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'products/:id', component: ProductsComponent,
+  },
+  {
+    path: 'cart/:id', component: CartComponent,
+  },
+  {
+    path: 'checkout/:id', component: CheckoutComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
