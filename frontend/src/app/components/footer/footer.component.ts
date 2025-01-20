6afefdd70e94e712f069d1d0aa725ce75003a637
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-01-20 17:19:08 +0530

    feat: Create Footer Section

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: false,
-  
+
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
